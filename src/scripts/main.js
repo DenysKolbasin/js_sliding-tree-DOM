627cfd3d8ea9a4e10ad06ded764d0dcadb43aaaa
Denys <kolbasin1990@gmail.com>
2023-05-16 22:05:27 +0300

    js_sliding_tree_DOM_v_01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach(item => {
+  const text = item.firstChild.textContent;
+  const span = document.createElement('span');
+  span.textContent = text;
+  item.firstChild.replaceWith(span);
+
+  span.addEventListener('click', event => {
+    const subTree = item.querySelector('ul');
+    if (subTree && event.target === span) {
+      subTree.style.display = subTree.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
+
+
+
+
+
+
+
+
